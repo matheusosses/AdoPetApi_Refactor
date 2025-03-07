61f48e746cc5fe40dc9aeb22812f817ec586e31d
Matheus Osses de Lima <maths0ss3s@gmail.com>
2025-03-07 16:36:47 -0300

    Refactor: Consultas otimizadas

--- src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComAdocaoEmAndamento.java
+++ src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComAdocaoEmAndamento.java
@@ -2,34 +2,23 @@ package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDTO;
 import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
-import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
-import br.com.alura.adopet.api.repository.TutorRepository;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class ValidacaoTutorComAdocaoEmAndamento implements IValidador{
 
     private final AdocaoRepository adocaoRepository;
-    private final TutorRepository tutorRepository;
 
-    public ValidacaoTutorComAdocaoEmAndamento(AdocaoRepository adocaoRepository, TutorRepository tutorRepository) {
+    public ValidacaoTutorComAdocaoEmAndamento(AdocaoRepository adocaoRepository) {
         this.adocaoRepository = adocaoRepository;
-        this.tutorRepository = tutorRepository;
+
     }
 
     public void validar(SolicitacaoAdocaoDTO dto) {
-        List<Adocao> adocoes = adocaoRepository.findAll();
-        Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
-
-        for (Adocao a : adocoes) {
-            if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
-            }
+        if (adocaoRepository.existsByTutorIdAndStatus(dto.idTutor(), StatusAdocao.AGUARDANDO_AVALIACAO)) {
+            throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
         }
     }
 }
